854a37a80d0ae3f188549c7ca793c2d852b4d88e
Daniel Suárez <dwsuarez98@gmail.com>
2023-02-01 22:48:19 -0300

    set e intersection

--- cheatsheet_python.py
+++ cheatsheet_python.py
@@ -76,13 +76,66 @@ class TestIsEven(unittest.TestCase):
 
 unittest.main()
 
+#################
+# Función REDUCE# 
+#################
+
+""" 
+reduce es una función en Python que permite reducir una secuencia (por ejemplo, 
+una lista) a un solo valor combinando elementos de la secuencia de forma secuencial. 
+La función reduce se encuentra en el módulo functools.
+
+La sintaxis de reduce es la siguiente:
+    from functools import reduce
+
+    reduce(function, iterable)
+
+function: es la función que se aplicará para reducir la secuencia. 
+La función debe aceptar dos argumentos y devolver un solo valor.
+iterable: es la secuencia que se reducirá.
+El resultado de reduce es el valor final después de aplicar la función 
+function a los elementos de la secuencia.
+
+Ejemplo:
+
+"""
+
+from functools import reduce
+
+def add(x, y):
+    return x + y
+
+numbers = [1, 2, 3, 4, 5]
+sum_of_numbers = reduce(add, numbers)
+print(sum_of_numbers) # 15
 
 
+#################
+# Función REDUCE# 
+#################
 
 
+""" 
+set es una estructura de datos en Python que permite almacenar valores únicos. 
+La función intersection se usa para calcular la intersección entre dos conjuntos, 
+es decir, para encontrar los valores que están presentes en ambos conjuntos.
 
+"""
+a = set([1, 2, 3, 4, 5])
+b = set([4, 5, 6, 7, 8])
 
+intersection = a.intersection(b)
+print(intersection) # {4, 5}
 
+class TestIntersection(unittest.TestCase):
+    def test_intersection(self):
+        a = set([1, 2, 3, 4, 5])
+        b = set([4, 5, 6, 7, 8])
+
+        intersection = a.intersection(b)
+        self.assertEqual(intersection, {4, 5})
+
+unittest.main()
 
 
 
