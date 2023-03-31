f9edb8ee4859577d7f664e0b782deb2bb719ed84
Daniel Suárez <dwsuarez98@gmail.com>
2023-03-31 19:53:02 -0300

    *args y **kwargs

--- cheatsheet_python.py
+++ cheatsheet_python.py
@@ -17,7 +17,7 @@ tupla, etc. utilizando la función list(), tuple(), etc.
 
 Ejemplo:
 """
-def cuadrado,x):
+def cuadrado(x):
     return x**2
 
 numeros = [1,2,4,5,6]
@@ -270,3 +270,44 @@ documentación y los argumentos.
 función y evitar que se vuelvan a calcular en futuras llamadas con los mismos 
 argumentos.
 """
+
+###########################
+# *args **kwargs
+###########################
+""" *args:
+
+El parámetro args se utiliza para pasar un número variable de argumentos 
+posicionales a una función. Los argumentos posicionales son aquellos que se 
+especifican en la llamada a la función, sin incluir los argumentos con nombre. 
+El asterisco () antes del nombre del parámetro indica que la función puede 
+recibir cualquier número de argumentos posicionales. Los argumentos se pasan
+como una tupla y se pueden acceder dentro de la función utilizando su índice 
+en la tupla."""
+
+def suma(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+
+print(suma(1, 2, 3))  # 6
+print(suma(4, 5, 6, 7))  # 22
+
+"""
+**kwargs:
+
+El parámetro kwargs se utiliza para pasar un número variable de argumentos 
+con nombre a una función. Los argumentos con nombre se especifican en la 
+llamada a la función utilizando el formato clave=valor. El doble asterisco () 
+antes del nombre del parámetro indica que la función puede recibir cualquier 
+número de argumentos con nombre. Los argumentos se pasan como un diccionario 
+y se pueden acceder dentro de la función utilizando la clave del diccionario.
+"""
+def imprimir_nombres(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+imprimir_nombres(nombre="Juan", apellido="Pérez", edad=30)
+# nombre: Juan
+# apellido: Pérez
+# edad: 30
