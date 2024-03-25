91d9f65b7d9e9faf9ba78ad53e53ef6caa826526
printdaniel <dwsuarez98@gmail.com>
2024-03-25 19:15:24 -0300

    update: yield

--- usar_yield.py
+++ usar_yield.py
@@ -14,3 +14,16 @@ mi_generador = generador()
 
 for valor in mi_generador:
     print(valor)
+
+
+
+def contador(n):
+    i = 0
+    while i < n:
+        yield i
+        i += 1
+
+
+
+for num in contador(18):
+    print(num)
